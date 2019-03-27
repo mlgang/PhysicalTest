887c4a1ab98497d33a158c002bac9288f411d1d0
jay <jay@cernet2.net>
2019-03-27 22:31:53 +0800

    Initial Commit

---
+++ PhysicalTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhysicalTest
+//
+//  Created by jay on 2019/3/27.
+//  Copyright © 2019 jay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhysicalTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhysicalTest
+//
+//  Created by jay on 2019/3/27.
+//  Copyright © 2019 jay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
